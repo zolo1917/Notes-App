e3fc48c954201a9ed861511cda67bcdc5000271e
LAPTOP-0D4JLI6O\adwai <adwaith_venkatesh@msn.com>
2021-03-14 20:37:09 +0530

    Yargs changes

--- app.js
+++ app.js
@@ -1,14 +1,50 @@
 // node npm validator version 10.8.0
 const validator = require('validator');
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getnotes = require ('./notes.js');
 
-if(process.argv[2] === 'add'){
-    console.log('adding note');
-}else if (process.argv[2] === 'remove'){
-    console.log('removing note')
-}
+
+// console.log(yargs.argv);
+
+//customize yargs version number
+yargs.version("1.1.0");
+
+//Create add
+yargs.command({
+    command: 'add',
+    description : 'Add a new note',
+    handler : function() {
+        console.log('adding note');
+    }
+}).parse();
+
+//Create Remove command
+yargs.command({
+    command: 'remove',
+    description : 'Remove a note',
+    handler : function() {
+        console.log('Removing note');
+    }
+}).parse();
+
+//Create Read
+yargs.command({
+    command: 'read',
+    description : 'Read an existing note',
+    handler : function() {
+        console.log('Reading note');
+    }
+}).parse();
+
+//Create List
+yargs.command({
+    command: 'list',
+    description : 'Listing all existing notes',
+    handler : function() {
+        console.log('Listing notes');
+    }
+}).parse();
 
 
-
-console.log(process.argv[2]);
+// console.log(process.argv[2]);
