84ddd95082c8420f8aaa11c04f1e16a2fca6c896
LAPTOP-0D4JLI6O\adwai <adwaith_venkatesh@msn.com>
2021-03-14 20:08:03 +0530

    Commiting new changes

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
-// const add = require('./utils')
-
 // node npm validator version 10.8.0
 const validator = require('validator');
 const chalk = require('chalk');
+const getnotes = require ('./notes.js');
+
+if(process.argv[2] === 'add'){
+    console.log('adding note');
+}else if (process.argv[2] === 'remove'){
+    console.log('removing note')
+}
 
 
 
-const getnotes = require ('./notes.js')
-
-// console.log(add(2,4));
-
-// console.log(getnotes());
-console.log(chalk.green.bold('Success!'))
-
-// console.log(validator.isEmail('adwaith_venkatesh@msn.com'));
-// console.log(validator.isURL('http://google.com'));
+console.log(process.argv[2]);
